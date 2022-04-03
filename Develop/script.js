b7dd1b1595f184351648d7f1a5a0d133befc0363
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-04-02 20:42:35 -0700

    got the data from Openweather API, displayed the date and location on screen, and set up the 5 day forecast presentation of data

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,14 @@ var submit = document.getElementById("button");
 var query = document.getElementById("query");
 var history = document.getElementById("history");
 var clear = document.getElementById("clear");
+var currentDate = document.getElementById("cdate");
+const d = new Date();
 
-function setFiveDay() {
+function setFiveDay(index) {
     var future = document.createElement("div");
     future.setAttribute("id","future");
     var h3 = document.createElement("h3");
-    h3.setAttribute("id", "date");
+    h3.setAttribute("id", `date${index}`);
     var img = document.createElement("img");
     var tempDiv = document.createElement("div");
     tempDiv.setAttribute("id", "title");
@@ -20,28 +22,40 @@ function setFiveDay() {
     var pWind = document.createElement("p");
     var pHumid = document.createElement("p");
     var temp = document.createElement("p");
-    temp.setAttribute("id","temp");
+    temp.setAttribute("id",`temp${index}`);
     var wind = document.createElement("p");
-    wind.setAttribute("id","wind");
+    wind.setAttribute("id",`wind${index}`);
     var humid = document.createElement("p");
-    humid.setAttribute("id","humid");
+    humid.setAttribute("id",`humid${index}`);
 
     fiveDayForecast.appendChild(future);
     future.appendChild(h3);
     future.appendChild(img);
     future.appendChild(tempDiv);
     tempDiv.appendChild(pTemp);
-    pTemp.textContent = "Temp: ";
+    pTemp.textContent = `Temp: `;
     tempDiv.appendChild(temp);
     future.appendChild(windDiv);
     windDiv.appendChild(pWind);
-    pWind.textContent = "Wind: ";
+    pWind.textContent = `Wind: `;
     windDiv.appendChild(wind);
     future.appendChild(humidDiv);
     humidDiv.appendChild(pHumid);
-    pHumid.textContent = "Humidity: ";
+    pHumid.textContent = `Humidity: `;
     humidDiv.appendChild(humid);
 }
+function setFiveDaystats(index,temperature,wind_speed,humidity,day) {
+    var temp = document.getElementById(`temp${index}`);
+    temp.textContent = ` ${temperature}`;
+    var wind = document.getElementById(`wind${index}`);
+    wind.textContent = ` ${wind_speed}`;
+    var humid = document.getElementById(`humid${index}`);
+    humid.textContent = ` ${humidity}`;
+    var date = document.getElementById(`date${index}`);
+    date.textContent = `${d.getMonth()+1}/${day}/${d.getFullYear()}`;
+
+}
+
 var histlist = [];
 console.log(histlist.length);
 function deleteHistory() {
@@ -49,11 +63,14 @@ function deleteHistory() {
     for (var i = history.getElementsByTagName('*').length; i > 0; i--) {
         console.log(history.getElementsByTagName('*').length);
         history.removeChild(history.firstChild);
-        console.log("hi");
     }
 }
+
 function setHistory(query) {
     deleteHistory();
+    searchQuery(query);
+    var city = document.getElementById("city");
+    city.textContent = query + " ";
     var history = document.getElementById("history");
     console.log(histlist.length);
     if (histlist.length == 0) {
@@ -91,12 +108,45 @@ function storingLocal() {
 
 function writeHistory() {
     var storedNames = JSON.parse(localStorage.getItem("names"));
-    for (var i = 0; i < storedNames.length; i++) {
-        setHistory(storedNames[i]);
+    if (storedNames) {
+        for (var i = 0; i < storedNames.length; i++) {
+            setHistory(storedNames[i]);
+        }
     }
 }
+
 function destroyHistory() {
     window.localStorage.clear();
+}
+
+function searchQuery(query) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=23551cfb9a93dcdae75a00d025e1f9f8`)
+        .then(function (response) {
+            response.json()
+                .then(function (response) {
+                    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${response[0].lat}&lon=${response[0].lon}&exclude=minutely,hourly,alerts&appid=23551cfb9a93dcdae75a00d025e1f9f8&units=imperial`)
+                    .then(function(response) {
+                        response.json()
+                        .then(function (response) {
+                            setCurrentWeather(response.current.temp,response.current.wind_speed,response.current.humidity,response.current.uvi);
+                            for (var i = 1; i < 6; i++) {
+                                setFiveDaystats(i,response.daily[i].temp.day,response.daily[i].wind_speed,response.daily[i].humidity,d.getDate()+i);
+                            }
+                        })
+                    })
+                })
+        })
+}
+
+function setCurrentWeather(temp, wind, humid, uv) {
+    var ctemp = document.getElementById("ctemp");
+    ctemp.textContent = temp;
+    var cwind = document.getElementById("cwind");
+    cwind.textContent = wind;
+    var chumid = document.getElementById("chumid");
+    chumid.textContent = humid;
+    var cuv = document.getElementById("cuv");
+    cuv.textContent = uv;
 }
 
 submit.addEventListener("click", function(){
@@ -110,5 +160,8 @@ document.getElementById("history").addEventListener("click", function(event){
 clear.addEventListener("click", function() {
     destroyHistory();
 })
-
+currentDate.textContent = `(${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()})`;
+for (var i = 1; i < 6; i++) {
+    setFiveDay(i);
+}
 writeHistory();
